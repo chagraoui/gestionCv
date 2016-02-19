dad83ca8654d0df6b991724aa6f91af6d13c29ae
mrmadou <mehdi.chagraoui@gmail.com>
2016-02-19 12:38:58 +0100

    version1

--- CvParsing/src/com/tritux/cvparsing/CV.java
+++ CvParsing/src/com/tritux/cvparsing/CV.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -15,24 +16,45 @@ public class CV {
 	private String certif;
 	private String adresse;
 	private String mail;
+	private String studies;
+	private Set<String> skills;
+	private String num;
+	private List<Map.Entry<String, Integer>> keywords;
+	
 	private BufferedReader in;
-
-
 	
 	
 	public CV() throws IOException {
 		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
 		this.age=GetAge.age(in);
+		
 		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
 		this.certif=GetCertif.certif(in);
+		
 		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
 		this.name=GetName.name(in);
+		
 		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
 		this.adresse=GetLocation.location(in);
+		
 		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
 		this.mail=GetMail.mail(in);
+		
+		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
+		this.studies=GetStudies.studies(in);
+		
+		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
+		this.skills=GetSkills.skills(in);
+		
+		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
+		this.num=GetPhoneNum.num(in);
+		
+		this.in= new BufferedReader(new FileReader("cvMehdi.txt"));
+		this.keywords=GetOccurrence.occ(in);
+		
+		
+		
 	}
-	
 
 
 	/* (non-Javadoc)
@@ -41,9 +63,12 @@ public class CV {
 	@Override
 	public String toString() {
 		return "CV [age=" + age + ", name=" + name + ", certif=" + certif
-				+ ", adresse=" + adresse + ", mail=" + mail + "]";
+				+ ", adresse=" + adresse + ", mail=" + mail + ", studies="
+				+ studies + ", skills=" + skills + ", num=" + num
+				+ ", keywords=" + keywords + "]";
 	}
 	
+
 	
 
 	
--- CvParsing/src/com/tritux/genererJson/yo.java
+++ CvParsing/src/com/tritux/genererJson/yo.java
@@ -1,41 +1,22 @@
 package com.tritux.genererJson;
 
-
-
-import java.io.FileWriter;
 import java.io.IOException;
-
 import com.google.gson.Gson;
 import com.tritux.cvparsing.CV;
 
 
 public class yo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 	
-    CV c = null;
-	try {
-		c = new CV();
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}
+    CV c = new CV();
+    System.out.println(c);
+	
 	Gson gson = new Gson();
 
 	// convert java object to JSON format,
 	// and returned as JSON formatted string
 	String json = gson.toJson(c);
-
-	try {
-		//write converted json data to a file named "file.json"
-		FileWriter writer = new FileWriter("file.json");
-		writer.write(json);
-		writer.close();
-
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-
 	System.out.println(json);
 
     }
